75fc63080836f9c04e78c2bebad2bac2dd8201c7
adamawalters <adamawalters@gmail.com>
2024-02-02 10:50:36 -0500

    footer on bottom and not sticky

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -13,7 +13,7 @@ const Search = ({ token }: SearchProps) => {
   const [selectedArtist, setSelectedArtist] = useState<null | Artist>(null);
 
 
-  /* TODO: add pagination so users can select between more than the top 5 artists that show up*/
+  /* TODO: add pagination so users can select between more than the top 5 artists that show up */
   async function handleSearch(searchKey: string) {
     const params = new URLSearchParams({
       q: searchKey,
@@ -35,28 +35,6 @@ const Search = ({ token }: SearchProps) => {
 
     setArtists(responseArtists);
   }
-
-  /* By default before user searches - load top artists*/
-  // useEffect(() => {
-  //   async function loadTopArtists() {
-  //     const response = await fetch(
-  //       `https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx,57dN52uHvrHOxijzpIgu3E,1vCWHaC5f2uS3yhpwWbIA6`,
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       }
-  //     );
-
-  //     const parsedResponse = await response.json();
-  //     console.log(parsedResponse);
-  //     const responseArtists = parsedResponse.artists as Array<Artist>;
-
-  //     setArtists(responseArtists);
-  //   }
-
-  //   loadTopArtists();
-  // }, [token]);
 
   return (
     <>
