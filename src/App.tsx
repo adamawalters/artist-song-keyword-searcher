7f03d783a15e883cf5574700dc57d41f00217205
adamawalters <adamawalters@gmail.com>
2024-02-11 19:35:08 -0500

    logout button only visible if logged in. moved header and footer to within AppRoutes component to get token state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
 import AppRoutes from './components/AppRoutes'
 
 
 const App = () => {
 
   return ( 
-    <>
-        <Header />
-        <AppRoutes />
-        <Footer />
-    </>
+    <AppRoutes />
   )
 }
 
